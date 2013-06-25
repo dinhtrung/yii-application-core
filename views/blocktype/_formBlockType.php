99714933f2c80530b3e8a61a759075e554526ed6
Nguyen Dinh Trung <nguyendinhtrung141@gmail.com>
2013-06-25 15:03:36 +0700

    Fix small in form block type

--- views/blocktype/_formBlockType.php
+++ views/blocktype/_formBlockType.php
@@ -1,4 +1,4 @@
-<?php echo TbHtml::well(Yii::t('core','Fields with <span class="required">*</span> are required.'));?>
+<?php echo TbHtml::well(Yii::t('app','Fields with <span class="required">*</span> are required.'));?>
 
 <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
 		'id'=>'blocktype-form',
@@ -7,18 +7,18 @@
 	echo $form->errorSummary($model);
 ?>
 
-	<?php echo $form->textFieldControlGroup($model,'btid',array('help'=> Yii::t('core', "Machine name of this block type."))); ?>
+	<?php echo $form->textFieldControlGroup($model,'btid',array('help'=> $model->t("Machine name of this block type."))); ?>
 
-	<?php echo $form->textFieldControlGroup($model,'title',array('help' => Yii::t('core', "Name of this block type. Used in administration interfaces"))); ?>
+	<?php echo $form->textFieldControlGroup($model,'title',array('help' => $model->t("Name of this block type. Used in administration interfaces"))); ?>
 
-	<?php echo $form->textAreaControlGroup($model,'description',array('cols' => 70, 'size' => TbHtml::INPUT_SIZE_XXLARGE, 'rows' => 5, 'help'=> Yii::t('core', 'You may use <a target="_blank" href="http://daringfireball.net/projects/markdown/syntax">Markdown syntax</a>.'))); ?>
+	<?php echo $form->textAreaControlGroup($model,'description',array('cols' => 70, 'size' => TbHtml::INPUT_SIZE_XXLARGE, 'rows' => 5, 'help'=> $model->t('You may use <a target="_blank" href="http://daringfireball.net/projects/markdown/syntax">Markdown syntax</a>.'))); ?>
 
-	<?php echo $form->textFieldControlGroup($model,'component',array('help'=> Yii::t('core', "Yii Component that provide Callback Data and Config functions, written in Yii path alias."))); ?>
+	<?php echo $form->textFieldControlGroup($model,'component',array('help'=> $model->t("Yii Component that provide Callback Data and Config functions, written in Yii path alias."))); ?>
 
-	<?php echo $form->textFieldControlGroup($model,'callback',array('help'=> Yii::t('core', "Prefix for the callback functions.<br> This block configuration will be provided by function <code><em>callback</em>Config</code> while data will be provided by <code><em>callback</em>Data</code> in the component above."))); ?>
+	<?php echo $form->textFieldControlGroup($model,'callback',array('help'=> $model->t("Prefix for the callback functions.<br> This block configuration will be provided by function <code><em>callback</em>Config</code> while data will be provided by <code><em>callback</em>Data</code> in the component above."))); ?>
 
 	<?php echo $form->textFieldControlGroup($model,'viewfile',array(
-			'help' => Yii::t('core', "The view file this block type will use. Provided as Yii path alias format.<br/>Finds a view file based on its name. The view name can be in one of the following formats:<br>
+			'help' => $model->t("The view file this block type will use. Provided as Yii path alias format.<br/>Finds a view file based on its name. The view name can be in one of the following formats:<br>
 	<ul>
 		<li><strong>absolute view within a module:</strong> the view name starts with a single slash <kbd>'/'</kbd>. In this case, the view will be searched for under the currently active module's view path. If there is no active module, the view will be searched for under the application's view path.</li>
 		<li><strong>absolute view within the application:</strong> the view name starts with double slashes <kbd>'//'</kbd>. In this case, the view will be searched for under the application's view path.</li>
